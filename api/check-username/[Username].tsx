cfaaaa1c56c402f0bac7e7bd1cb6fc5d6077b81f
sebastianf777 <sebastianf777@outlook.com>
2025-03-15 13:06:20 +0100

    testing

--- api/check-username/[Username].tsx
+++ api/check-username/[Username].tsx
@@ -2,7 +2,7 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
     // En Vercel los parámetros se obtienen de req.query
-    const { myUsername } = req.query;
-    console.log(`Received username: ${myUsername}`);
-    res.status(200).json({ check: true, username: 'myUsername' });
+    const { Username } = req.query;
+    console.log(`Received username: ${Username}`);
+    res.status(200).json({ check: true, username: Username });
 }
