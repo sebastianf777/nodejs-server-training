c4f7fbefec23260e4e65f7437e17c38807c69de0
sebastianf777 <sebastianf777@outlook.com>
2025-03-25 22:10:46 +0100

    testing

--- app/logout/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from 'react'
-
-import '../../styles/logout.styles.scss'
-
-export default function LogoutLayout({ children }: { children: ReactNode }) {
-  return <div className={'layout-logout'}>{children}</div>
-}
--- components/utils/utils.constants.ts
+++
@@ -1,6 +0,0 @@
-export enum LOCAL_STORAGE {
-  USERNAME = 'username',
-  SESSION_TIMESTAMP = 'sessionTimestamp',
-}
-
-export const SESSION_EXPIRATION_TIME = 5 * 60 * 1000
