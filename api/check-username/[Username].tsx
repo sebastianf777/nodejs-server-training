cb4deff9ee6595610972d80c1a4748dfaf3a7547
sebastianf777 <sebastianf777@outlook.com>
2025-03-25 21:19:51 +0100

    code fixes

--- api/check-username/[Username].tsx
+++ api/check-username/[Username].tsx
@@ -2,6 +2,7 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
     // En Vercel los parámetros se obtienen de req.query
+    res.setHeader('Access-Control-Allow-Origin', '*')
     const { Username } = req.query;
     console.log(`Received username: ${Username}`);
     res.status(200).json({ check: true, username: Username });
