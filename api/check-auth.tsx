2938206d7bf9d6f375f330dbadd82779038f41d5
sebastianf777 <sebastianf777@outlook.com>
2025-03-26 12:01:59 +0100

    fixing code policy

--- api/check-auth.tsx
+++ api/check-auth.tsx
@@ -4,6 +4,7 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
 
     const { username, password } = req.body;
     console.log(`Authenticating user: ${username}`);
+    res.setHeader('Access-Control-Allow-Origin', '*')
 
     if (password === "777Holis") {
         const dummyToken = "abc123";
